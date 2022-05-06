d90de8bf4d6985233f800c93a3de472fc1295494
Posonap <76529412+posonap@users.noreply.github.com>
2022-05-06 19:34:21 +0300

    vestion 0.1

---
+++ src/model/GenericEnitity.java
@@ -0,0 +1,6 @@
+package model;
+
+public class GenericEnitity {
+
+    Integer id;
+}
---
+++ src/model/User.java
@@ -0,0 +1,7 @@
+package model;
+
+public class User {
+    long i;
+    String firstname;
+    String lastname;
+}
