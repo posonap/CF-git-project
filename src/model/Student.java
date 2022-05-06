e6edac630b38e6b409201408defe701428a6a9d9
Posonap <76529412+posonap@users.noreply.github.com>
2022-05-06 19:38:11 +0300

    Version 0.2

---
+++ src/model/Student.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Student {
+    String firstname;
+    String lastname;
+}
---
+++ src/model/Teacher.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Teacher {
+
+    String firstname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    long id;
+}
